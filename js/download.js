5f7822fc01b69499a2efce518d59c2e9f2f6f707
LinkTech-Zamboanga <307185229+linktechzamboanga-lang@users.noreply.github.com>
2026-08-25 14:01:12 +0800

    Refactor download button system and add links

--- js/download.js
+++ js/download.js
@@ -52,103 +52,146 @@ function searchSoftware(){
    DOWNLOAD BUTTON SYSTEM
 ========================= */
 
+const downloadFiles = {
+
+    linktech2026:
+        "https://drive.usercontent.google.com/download?id=1SsUikiB9N8Nw0qADAVar7rxqRYt_OG0K&export=download&authuser=0"
+        
+    Winrar:
+    "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-722.exe"
+
+Chrome:
+"https://dl.google.com/chrome/install/latest/chrome_installer.exe"
+
+VLC:
+"https://get.videolan.org/vlc/3.0.23/win64/vlc-3.0.23-win64.exe"
+
+Epson L120 Resetter:
+"https://ozamiz.deped.gov.ph/resetter/l120-resetcracked.zip"
+
+
+Epson L3150 Resetter:
+"https://ozamiz.deped.gov.ph/resetter/Epson%20L3150%20Resetter.zip"
+
+Epson L1210 / L3210 / L3250 Series Resetter:
+"https://ozamiz.deped.gov.ph/resetter/EPSON_L1210_L3210_L3250_L3251_L3260_L5290.zip"
+
+
+AnyDesk:
+"https://download.anydesk.com/AnyDesk.exe"
+
+
+};
+
+
+/* =========================
+   DOWNLOAD BUTTON SYSTEM
+========================= */
 
 const downloadButtons =
 document.querySelectorAll(".download-btn");
 
 
+downloadButtons.forEach(button => {
 
-downloadButtons.forEach(button=>{
+    button.addEventListener("click", function () {
 
+        const file =
+            this.getAttribute("data-file");
 
-button.addEventListener("click",function(){
-
-
-    let file =
-    this.getAttribute("data-file");
-
-
-    let text =
-    this.querySelector(".btn-text");
+        const text =
+            this.querySelector(".btn-text");
 
 
+        /* =========================
+           START LOADING
+        ========================= */
 
-    /* START LOADING */
-
-
-    this.classList.add("downloading");
-
-
-    text.innerText =
-    "Preparing Download...";
-
-
-
-    /*
-       OPEN DOWNLOAD
-       New tab prevents
-       browser blocking
-    */
-
-
-    setTimeout(()=>{
-
-
-        window.open(
-            file,
-            "_blank"
-        );
-
-
-    },800);
-
-
-
-
-    /* SUCCESS MESSAGE */
-
-
-    setTimeout(()=>{
-
-
-        this.classList.remove(
-            "downloading"
-        );
-
-
-        this.classList.add(
-            "success"
-        );
-
+        this.classList.add("downloading");
 
         text.innerText =
-        "Download Started";
+            "Preparing Download...";
 
 
-    },1500);
+        /* =========================
+           CREATE HIDDEN DOWNLOAD LINK
+        ========================= */
+
+        const downloadLink =
+            document.createElement("a");
+
+        downloadLink.href = file;
+
+        downloadLink.download = "";
 
 
+        /*
+         * Hide the link completely
+         */
+
+        downloadLink.style.display = "none";
 
 
-    /* RESET BUTTON */
-
-
-    setTimeout(()=>{
-
-
-        this.classList.remove(
-            "success"
+        document.body.appendChild(
+            downloadLink
         );
 
 
-        text.innerText =
-        "Download";
+        /* =========================
+           START DOWNLOAD
+        ========================= */
+
+        downloadLink.click();
 
 
-    },5000);
+        /* =========================
+           REMOVE TEMPORARY LINK
+        ========================= */
+
+        setTimeout(() => {
+
+            document.body.removeChild(
+                downloadLink
+            );
+
+        }, 1000);
 
 
+        /* =========================
+           SUCCESS
+        ========================= */
 
-});
+        setTimeout(() => {
+
+            this.classList.remove(
+                "downloading"
+            );
+
+            this.classList.add(
+                "success"
+            );
+
+            text.innerText =
+                "Download Started";
+
+        }, 1500);
+
+
+        /* =========================
+           RESET BUTTON
+        ========================= */
+
+        setTimeout(() => {
+
+            this.classList.remove(
+                "success"
+            );
+
+            text.innerText =
+                "Download";
+
+        }, 5000);
+
+    });
 
 });
