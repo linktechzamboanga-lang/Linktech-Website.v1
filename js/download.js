6b09d2de4a4bdb9b986e6255777124c423b1e53a
LinkTech-Zamboanga <307185229+linktechzamboanga-lang@users.noreply.github.com>
2026-08-26 23:58:30 +0800

    Add download link for Computer Inventory System pro

--- js/download.js
+++ js/download.js
@@ -84,7 +84,10 @@ const downloadFiles = {
         "https://download.anydesk.com/AnyDesk.exe",
    
    "Computer Inventory System":
-   "https://drive.usercontent.google.com/download?id=119p0k-lYEs9qANKXdwzrvvAArjvSpfW7&export=download&authuser=0"
+   "https://drive.usercontent.google.com/download?id=119p0k-lYEs9qANKXdwzrvvAArjvSpfW7&export=download&authuser=0",
+   
+   "Computer Inventory System pro":
+   "https://drive.usercontent.google.com/download?id=1KkyHHNZPZSLTa0HMXSbfrlNvMo8G6OYE&export=download&authuser=0"
 
 };
 
