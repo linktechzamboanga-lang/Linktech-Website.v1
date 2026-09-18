24bfc3d8609a49ca3189be0933e8c73faf499ca7
LinkTech-Zamboanga <307185229+linktechzamboanga-lang@users.noreply.github.com>
2026-09-18 14:48:00 +0800

    Fix syntax error in download.js

--- js/download.js
+++ js/download.js
@@ -36,7 +36,7 @@ const downloadFiles = {
 
 
     AnyDesk:
-        "https://download.anydesk.com/AnyDesk.exe"
+        "https://download.anydesk.com/AnyDesk.exe",
         
     Moviebox:
         "https://www.mediafire.com/file/14kayxhxr1xqgsd/com.community.mbox.tv-release-1.1.10.0901-of-50040016.apk/file?dkey=9easz4hwowd&r=179",
